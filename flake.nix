83596f2bd5c3b27ae8b1f03e9aea2a93868588ab
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-02-25 20:46:26 +0100

    chore: fix typos in flake.nix

--- flake.nix
+++ flake.nix
@@ -10,34 +10,43 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, ...}: {
-    nixosConfigurations.default = nixos.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # "pkgs.unstable" overlay
-        {
-          nixpkgs.overlays = [
-            (final: prev: {
-              unstable = import nixpkgs-unstable {
-                inherit (final) config;
-                inherit (final.stdenv.hostPlatform) system;
-              };
-            })
-          ];
-        }
-
-        # config
-        ./configuration.nix
-
-        # home-manager
-        home-manager.nixosModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.skar = import ./home.nix;
-            backupFileExtension = "backup";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      ...
+    }:
+    {
+      nixosConfigurations.default = nixos.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # "pkgs.unstable" overlay
+          {
+            nixpkgs.overlays = [
+              (final: prev: {
+                unstable = import nixpkgs-unstable {
+                  inherit (final) config;
+                  inherit (final.stdenv.hostPlatform) system;
+                };
+              })
+            ];
           }
-        }
-      ];
+
+          # config
+          ./configuration.nix
+
+          # home-manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.skar = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
     };
 }
