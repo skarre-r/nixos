7d6fdf2cab80108be59bf82b8de70699b41cf891
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-02-27 12:43:58 +0100

    chore(modules/system): update included/ excluded gnome packages

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -2,25 +2,68 @@
   pkgs,
   ...
 }:
+let
+  excludePackages = with pkgs; [
+    gnome-characters
+    gnome-clocks
+    gnome-console
+    gnome-font-viewer
+    gnome-logs
+    gnome-music
+    gnome-system-monitor
+    gnome-weather
+    gnome-connections # remote desktop
+    baobab # disk usage analyzer
+    decibels # audio player
+    epiphany # web browser
+    loupe # image viewer
+    showtime # video player
+    simple-scan # document scanner
+    snapshot # camera
+    yelp # help viewer
+    gnome-tour
+    gnome-user-docs
+    # gnome-text-editor
+    # gnome-calculator
+    # gnome-calendar
+    # gnome-contacts
+    # gnome-maps
+    # nautilus # file manager
+    # papers # document viewer
+    nixos-render-docs
+  ];
+
+  extraPackages = with pkgs; [
+    newsflash # rss reader
+    fragments # torrent client
+    errands # reminders app
+    blanket # ambient sounds
+    dialect # translation
+    iotas # note-taking app
+    # komikku # comic reader
+    dconf-editor
+  ];
+in
 
 {
   services.displayManager.gdm = {
     enable = true;
     wayland = true;
+    autoSuspend = true;
   };
   services.desktopManager.gnome.enable = true;
 
+  services.flatpak.enable = true;
+
   services.gnome = {
     core-apps.enable = true;
-    core-developer-tools.enable = true;
+    core-developer-tools.enable = false;
     games.enable = false;
+    gnome-online-accounts.enable = true;
   };
 
-  environment.gnome.excludePackages = with pkgs; [
-    gnome-tour
-    gnome-weather
-    gnome-console
-  ];
+  environment.gnome.excludePackages = excludePackages;
+  environment.systemPackages = extraPackages;
 
   programs.dconf.profiles.user.databases = [
     {
