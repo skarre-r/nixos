8fb86d0d3e3741bd3ab685a3730da2392025d4a6
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-02-27 14:28:57 +0100

    chore(home.nix): add font and editor config; prepare calendar/ contacts accounts

--- home.nix
+++ home.nix
@@ -17,5 +17,49 @@
     bruno
     libreoffice
     high-tide # 3rd party tidal client
+    gh
   ];
+
+  fonts.fontconfig = {
+    enable = true;
+    antialiasing = true;
+    hinting = "slight"; # null | "none" | "slight" | "medium" | "full"
+    subpixelRendering = "rgb"; # null | "none" | "rgb" | "bgr" | "vertical-rgb" | "vertical-bgr"
+  };
+
+  editorConfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        trim_trailing_whitespace = true;
+        insert_final_newline = true;
+      };
+    };
+  };
+
+  # TODO:
+  accounts = {
+    calendar.accounts."nextcloud" = {
+      name = "nextcloud";
+      primary = true;
+      remote = {
+        type = "caldav";
+        url = null;
+        username = null;
+        passwordCommand = "";
+      };
+    };
+    contacts.accounts."nextcloud" = {
+      name = "nextcloud";
+      remote = {
+        type = "carddav";
+        url = null;
+        username = null;
+        passwordCommand = null;
+      };
+    };
+  };
+
 }
