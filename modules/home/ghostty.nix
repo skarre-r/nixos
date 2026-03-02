2467229a6689b68ccdb1520a8884b10f0b11e309
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-03-02 14:32:35 +0100

    chore(modules/home): add more ghostty settings

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -4,15 +4,29 @@
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
+    clearDefaultKeybinds = false;
+    installBatSyntax = false;
+    installVimSyntax = false;
     settings = {
       font-family = "JetBrains Mono";
-      font-size = 13;
+      font-size = 12;
       font-thicken = true;
       theme = "Monokai Pro";
+      keybind = [ ];
       window-padding-x = 16;
       window-padding-y = 14;
       window-padding-balance = true;
       window-padding-color = "extend";
+      window-vsync = true;
+      window-inherit-working-directory = true;
+      window-decoration = "auto";
+      focus-follows-mouse = true;
+      quick-terminal-position = "top";
+      quick-terminal-autohide = false;
+      quick-terminal-keyboard-interactivity = "on-demand";
+      gtk-quick-terminal-layer = "top";
+      shell-integration = "detect";
     };
+    systemd.enable = true;
   };
 }
