df40fb4a7b2f22269f0fb3de37a38b8e782d1b16
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-03-02 14:15:27 +0100

    chore(home.nix): add vim settings/ config

--- home.nix
+++ home.nix
@@ -80,6 +80,35 @@ in
   programs.vim = {
     enable = true;
     defaultEditor = true;
+    settings = {
+      background = "dark";
+      number = true;
+      relativenumber = false;
+      ignorecase = true;
+      smartcase = true;
+    };
+    extraConfig = ''
+      filetype on
+      filetype plugin on
+      filetype indent on
+      syntax on
+      set cursorline
+      set shiftwidth=4
+      set tabstop=4
+      set expandtab
+      set nobackup
+      set scrolloff=10
+      set nowrap
+      set incsearch
+      set showcmd
+      set showmode
+      set showmatch
+      set hlsearch
+      set history=1000
+      set wildmenu
+      set wildmode=list:longest
+      set wildignore=*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx
+    '';
   };
   programs.vscode = {
     enable = true;
