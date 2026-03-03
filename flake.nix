1cdae6cd6c6ee8da39f61498be1a59e17f26b50f
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-03-03 10:12:34 +0100

    chore(flake.nix): remove nix-flatpak input; add nvf input

--- flake.nix
+++ flake.nix
@@ -8,7 +8,10 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -17,44 +20,37 @@
       nixpkgs,
       nixpkgs-unstable,
       home-manager,
-      nix-flatpak,
       ...
-    }:
+    }@inputs:
     {
-      nixosConfigurations = {
-        "thinkpad" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            # "pkgs.unstable" overlay
-            {
-              nixpkgs.overlays = [
-                (final: prev: {
-                  unstable = import nixpkgs-unstable {
-                    inherit (final) config;
-                    inherit (final.stdenv.hostPlatform) system;
-                  };
-                })
-              ];
-            }
-
-            # flatpaks
-            nix-flatpak.nixosModules.nix-flatpak
-
-            # config
-            ./configuration.nix
-
-            # home-manager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.skar = import ./home.nix;
-                backupFileExtension = "backup";
-              };
-            }
-          ];
-        };
+      nixosConfigurations."thinkpad" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          {
+            # TODO: move this somewhere else?
+            nixpkgs.overlays = [
+              (final: prev: {
+                unstable = import nixpkgs-unstable {
+                  inherit (final) config;
+                  inherit (final.stdenv.hostPlatform) system;
+                };
+              })
+            ];
+          }
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.skar = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              system = "x86_64-linux";
+            };
+          }
+        ];
       };
     };
 }
