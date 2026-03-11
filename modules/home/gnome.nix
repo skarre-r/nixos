96f1ab322b1d0c0e6e05a05fac66c3884a0030a4
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-03-11 17:28:17 +0100

    chore(modules/home): add gnome dconf settings

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -29,8 +29,85 @@ in
       "org/gnome/desktop/calendar" = {
         show-weekdate = true;
       };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        enable-animations = false;
+        enable-hot-corners = false;
+        font-antialiasing = "rgba";
+        font-hinting = "full";
+        show-battery-percentage = true;
+        text-scaling-factor = 1.0;
+      };
+      "org/gnome/desktop/wm/keybindings" = {
+        maximize = [];
+        minimize = [];
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,maximize,close";
+        num-workspaces = 1;
+      };
       "org/gnome/mutter" = {
+        dynamic-workspaces = false;
+        edge-tiling = true;
         overlay-key = "";
+      };
+      "org/gnome/mutter/keybinding" = {
+        toggle-tiled-left = ["<Super>Left"];
+        toggle-tiled-right = ["<Super>Right"];
+      };
+      "org/gnome/nautilus/preferences" = {
+        date-time-format = "simple";
+        default-folder-viewer = "list-view";
+        show-image-thumbnails = "always";
+      };
+      "org/gnome/shell" = {
+        enabled-extensions = [
+          "blur-my-shell@aunetx"
+          "just-perfection-desktop@just-perfection"
+          "dash-to-dock@micxgx.gmail.com"
+          "user-theme@gnome-shell-extensions.gcampax.github.com"
+          "gTile@vibou"
+          "kiwi@kemma"
+          "appindicatorsupport@rgcjonas.gmail.com"
+          "run-or-raise@edvard.cz"
+        ];
+      };
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        show-mounts = false;
+        show-show-apps-button = true;
+        show-trash = false;
+      };
+      "org/gnome/shell/extensions/gtile" = {
+        auto-close = true;
+        follow-cursor = false;
+        grid-sizes = "20x16";
+        insets-primary-bottom = 8;
+        insets-primary-left = 8;
+        insets-primary-right = 8;
+        insets-primary-top = 8;
+        insets-secondary-bottom = 8;
+        insets-secondary-left = 8;
+        insets-secondary-right = 8;
+        insets-secondary-top = 8;
+        show-icon = false;
+        window-spacing = 8;
+      };
+      # "org/gnome/shell/extensions/just-perfection" = {};
+      "org/gnome/shell/extensions/kiwi" = {
+        add-username-to-quick-menu = false;
+        battery-percentage = true;
+        button-type = "titlebuttons-alt";
+        enable-firefox-styling = true;
+        enable-launchpad-app = false;
+        hide-activities-button = false;
+        lock-icon = false;
+        move-calendar-right = false;
+        move-window-to-new-workspace = false;
+        skip-overview-on-login = true;
+        transparent-move = false;
+      };
+      "org/gtk/gtk4/settings/file-chooser" = {
+        show-hidden = true;
       };
     };
   };
@@ -50,7 +127,7 @@ in
   xdg.configFile."run-or-raise/shortcuts.conf".text = ''
     <Shift><Ctrl><Super><Alt>g,ghostty,,
     <Shift><Ctrl><Super><Alt>f,nautilus,Org.gnome.Nautilus,
-    <Shift><Ctrl><Super><Alt>z,zed,,
+    <Shift><Ctrl><Super><Alt>z,zeditor,Zed,
     <Shift><Ctrl><Super><Alt>s,slack,,
     <Shift><Ctrl><Super><Alt>p,plex,,
     <Shift><Ctrl><Super><Alt>o,obsidian,,
