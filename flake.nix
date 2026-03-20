0246563b9b7ec8f3ebe0f7cf0fae43b0378b2e0d
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-03-20 09:50:58 +0100

    chore(flake.nix): add nix user repository input; tweak the nixosConfigurations block

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nix user repository
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,34 +28,39 @@
       ...
     }@inputs:
     {
-      nixosConfigurations."thinkpad" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          {
-            # TODO: move this somewhere else?
-            nixpkgs.overlays = [
-              (final: prev: {
-                unstable = import nixpkgs-unstable {
-                  inherit (final) config;
-                  inherit (final.stdenv.hostPlatform) system;
-                };
-              })
+      nixosConfigurations = {
+        "thinkpad" =
+          let
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs system; };
+          in
+          nixpkgs.lib.nixosSystem {
+            inherit specialArgs;
+            modules = [
+              {
+                # TODO: move this somewhere else?
+                nixpkgs.overlays = [
+                  inputs.nur.overlays.default # TODO does this work?
+                  (final: prev: {
+                    unstable = import nixpkgs-unstable {
+                      inherit (final) config;
+                      inherit (final.stdenv.hostPlatform) system;
+                    };
+                  })
+                ];
+              }
+              ./configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.skar = import ./home.nix;
+                home-manager.backupFileExtension = "backup";
+                home-manager.extraSpecialArgs = specialArgs;
+              }
             ];
-          }
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.skar = import ./home.nix;
-            home-manager.backupFileExtension = "backup";
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              system = "x86_64-linux";
-            };
-          }
-        ];
+          };
       };
+
     };
 }
