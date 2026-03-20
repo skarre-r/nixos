aa38e2117b75fd45480b5cc62d3e7ac4f11d96d9
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-03-20 09:51:44 +0100

    chore(modules/home): add librewolf profile/ extensions

--- modules/home/librewolf.nix
+++ modules/home/librewolf.nix
@@ -4,7 +4,18 @@
   programs.librewolf = {
     enable = true;
     policies = { }; # TODO: extensions
-    profiles = { };
+    profiles = {
+      default = {
+        extensions = {
+          packages = with pkgs.nur.repos.rycee.firefox-addons; [
+            darkreader
+            multi-account-containers
+            onepassword-password-manager
+            ublock-origin
+          ];
+        };
+      };
+    };
     settings = { }; # TODO
     package = pkgs.librewolf.overrideAttrs (p: rec {
       desktopItem = p.desktopItem.override (i: {
