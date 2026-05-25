0bc682e431ff3b0014445a9f855df9c5af4204fd
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-05-25 11:37:46 +0200

    chore(modules/home): use different git user name & email based on remote

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -11,5 +11,25 @@
       };
       init.defaultBranch = "main";
     };
+    includes = {
+      github = {
+        condition = "hasconfig:remote.*.url:git@github.com:*";
+        contents = {
+          user = {
+            name = "skarre-r";
+            email = "59067313+skarre-r@users.noreply.github.com";
+          };
+        };
+      };
+      codeberg = {
+        condition = "hasconfig:remote.*.url:git@codeberg.org:*";
+        contents = {
+          user = {
+            name = "skarre-r";
+            email = "skarre-r@noreply.codeberg.org";
+          };
+        };
+      };
+    };
   };
 }
