65164f688aa20bd905b51faf9a4ea4f29d44ddfc
skarre-r <59067313+skarre-r@users.noreply.github.com>
2026-05-26 23:43:36 +0200

    chore(modules/home): fix different git user name & email based on remote

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -5,31 +5,49 @@
   programs.git = {
     enable = true;
     settings = {
-      user = {
-        name = "skarre-r";
-        email = "59067313+skarre-r@users.noreply.github.com";
-      };
+      # user = {
+      #   name = "skarre-r";
+      #   email = "59067313+skarre-r@users.noreply.github.com";
+      # };
       init.defaultBranch = "main";
     };
-    includes = {
-      github = {
-        condition = "hasconfig:remote.*.url:git@github.com:*";
+    includes = [
+      {
+        condition = "hasconfig:remote.*.url:git@github.com:*/**";
         contents = {
           user = {
             name = "skarre-r";
             email = "59067313+skarre-r@users.noreply.github.com";
           };
         };
-      };
-      codeberg = {
-        condition = "hasconfig:remote.*.url:git@codeberg.org:*";
+      }
+      {
+        condition = "hasconfig:remote.*.url:https://github.com/**";
+        contents = {
+          user = {
+            name = "skarre-r";
+            email = "59067313+skarre-r@users.noreply.github.com";
+          };
+        };
+      }
+      {
+        condition = "hasconfig:remote.*.url:git@codeberg.org:*/**";
         contents = {
           user = {
             name = "skarre-r";
             email = "skarre-r@noreply.codeberg.org";
           };
         };
-      };
-    };
+      }
+      {
+        condition = "hasconfig:remote.*.url:https://codeberg.org/**";
+        contents = {
+          user = {
+            name = "skarre-r";
+            email = "skarre-r@noreply.codeberg.org";
+          };
+        };
+      }
+    ];
   };
 }
